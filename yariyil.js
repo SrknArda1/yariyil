3e167f75e750860d971a44655e43971af7de6da8
SrknArda1 <ardasrk06@gmail.com>
2026-03-28 21:46:06 +0300

    Add files via upload

--- yariyil.js
+++ yariyil.js
@@ -1,8 +1,9 @@
 const basliklar = document.querySelectorAll(".baslik-1");
 basliklar.forEach(function(eleman) {
-    eleman.addEventListener("click", function() {
+    const ilkYuvarlak = eleman.querySelector(".yuvarlak");
+    ilkYuvarlak.addEventListener("click", function() {
         const icerik = eleman.querySelector(".icerik");
-        const ok = eleman.querySelector(".baslik h1 i");
+        const ok = ilkYuvarlak.querySelector("i");
         if (icerik.style.display === "none" || icerik.style.display === "") {
             icerik.style.display = "block";
             ok.style.transform = "rotate(90deg)";
